aa13b09a7b32eb51a7a6bc75dad7e719d01abce7
KVexcavator <str_token@mail.ru>
2018-10-24 08:01:46 +0600

    add test for power_up

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,9 +1,14 @@
 require './hero'
 
 describe  Hero do
-  it "has a capitalized name" do
+  it "has a capitalized name" do # название теста
     hero=Hero.new 'joker'
-
-    expect(hero.name).to eq 'Joker'
+    expect(hero.name).to eq 'Joker' # ожидаемый результат
   end
+
+  it "can power up" do
+    hero=Hero.new 'joker'
+    expect(hero.power_up).to eq 110
+  end
+
 end
