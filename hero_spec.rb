46aae86dc11a28bdf00218703835b71aa15fcd5d
KVexcavator <str_token@mail.ru>
2018-10-25 08:00:40 +0600

    refactoring tests

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,24 +1,25 @@
 require './hero'
 
 describe  Hero do
+
+  before do
+    @hero=Hero.new 'joker'
+  end
+
   it "has a capitalized name" do # название теста
-    hero=Hero.new 'joker'
-    expect(hero.name).to eq 'Joker' # ожидаемый результат
+    expect(@hero.name).to eq 'Joker' # ожидаемый результат
   end
 
   it "can power up" do
-    hero=Hero.new 'joker'
-    expect(hero.power_up).to eq 110
+    expect(@hero.power_up).to eq 110
   end
 
   it "can power down" do
-    hero=Hero.new 'joker'
-    expect(hero.power_down).to eq 90
+    expect(@hero.power_down).to eq 90
   end
 
   it "display full hero info" do
-    hero=Hero.new 'joker'
-    expect(hero.hero_info).to eq "Joker has a health of 100"
+    expect(@hero.hero_info).to eq "Joker has a health of 100"
   end  
 
 end
