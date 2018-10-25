7832f25532c1777ba91378575267f7749cd87b43
KVexcavator <str_token@mail.ru>
2018-10-25 07:51:54 +0600

    add power_down and hero_info tests

--- hero_spec.rb
+++ hero_spec.rb
@@ -11,4 +11,14 @@ describe  Hero do
     expect(hero.power_up).to eq 110
   end
 
+  it "can power down" do
+    hero=Hero.new 'joker'
+    expect(hero.power_down).to eq 90
+  end
+
+  it "display full hero info" do
+    hero=Hero.new 'joker'
+    expect(hero.hero_info).to eq "Joker has a health of 100"
+  end  
+
 end
